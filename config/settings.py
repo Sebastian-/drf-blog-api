26afb3de727fdf6cd8fbef9f0836683141fb3697
sebastian murgu <hmurgu@hotmail.com>
2021-08-03 14:35:58 -0700

    add rest framework and configure for token auth

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd Party
+    'rest_framework',
+    'rest_framework.authtoken',
+
     # Local
     'apps.users',
 ]
@@ -127,3 +131,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
