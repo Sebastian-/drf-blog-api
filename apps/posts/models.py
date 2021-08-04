495fb91442510e4b77563ac52d4b90ca5b9ece75
sebastian murgu <hmurgu@hotmail.com>
2021-08-04 12:14:52 -0700

    define post and viewcount models

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_edited = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(), related_name='posts', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['time_created']
+
+
+class ViewCount(models.Model):
+    count = models.PositiveIntegerField('view count', default=0)
+    post = models.OneToOneField(
+        Post, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return f'${str(self.count)} views for ${self.post.title}'
