bbae4aeb90d7fb70472fcf770b3a4557f8ea6879
sebastian murgu <hmurgu@hotmail.com>
2021-08-04 15:17:27 -0700

    update edit time whenever a post is edited

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -14,6 +15,16 @@ class Post(models.Model):
     author = models.ForeignKey(get_user_model(),
                                related_name='posts',
                                on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        """
+        Update time_edited whenever the post is edited
+        """
+        if self.id:
+            # only update if post already exists
+            self.time_edited = timezone.now()
+
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
