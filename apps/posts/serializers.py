ed4754316e1066d1ddd50bee31096cf55bb65061
sebastian murgu <hmurgu@hotmail.com>
2021-08-05 14:13:10 -0700

    create post serializers

--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -1,12 +1,23 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostListSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     viewcount = serializers.ReadOnlyField(source='viewcount.count')
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
     class Meta:
         model = Post
         fields = ['id', 'title', 'time_created',
-                  'time_edited', 'author', 'viewcount']
+                  'time_edited', 'author', 'viewcount', 'body']
+        extra_kwargs = {'body': {'write_only': True}}
+
+
+class PostDetailSerializer(PostListSerializer):
+
+    class Meta(PostListSerializer.Meta):
+        extra_kwargs = {}
